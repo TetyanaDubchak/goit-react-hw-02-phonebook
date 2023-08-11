9e61bc0a3f6f154ca2459dc1c8796bbd99a8df59
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2023-08-11 22:54:24 +0300

    add link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component} from "react";
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+
+export class App extends Component {
+
+
+   render() {
+    return (
+<></>
+    );
+  }
+}
